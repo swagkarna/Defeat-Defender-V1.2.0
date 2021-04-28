26feaa404a5f5260080c640cbb9f484708240bf1
swagkarna <46685308+swagkarna@users.noreply.github.com>
2021-04-28 22:32:56 +0530

    Update Enable Smart Screen.bat

--- Enable Smart Screen.bat
+++ Enable Smart Screen.bat
@@ -1,3 +1,28 @@
-takeown /f "%systemroot%\System32\smartscreen.exe" /a
-icacls "%systemroot%\System32\smartscreen.exe" /reset
-icacls "%systemroot%\System32\smartscreen.exe" /setowner *S-1-5-80-956008885-3418522649-1831038044-1853292631-2271478464
+@echo on
+:: BatchGotAdmin
+::-------------------------------------
+REM  --> Check for permissions
+>nul 2>&1 "%SYSTEMROOT%\system32\cacls.exe" "%SYSTEMROOT%\system32\config\system"
+
+REM --> If error flag set, we do not have admin.
+if '%errorlevel%' NEQ '0' (
+    echo Requesting administrative privileges...
+    goto UACPrompt
+) else ( goto gotAdmin )
+
+:UACPrompt
+    echo Set UAC = CreateObject^("Shell.Application"^) > "%temp%\getadmin.vbs"
+    set params = %*:"="
+    echo UAC.ShellExecute "cmd.exe", "/c %~s0 %params%", "", "runas", 1 >> "%temp%\getadmin.vbs"
+
+    "%temp%\getadmin.vbs"
+    del "%temp%\getadmin.vbs"
+    exit /B
+
+:gotAdmin
+cd %temp%
+NSudo.exe -U:T takeown /f "%systemroot%\System32\smartscreen.exe" /a
+
+NSudo.exe -U:T icacls "%systemroot%\System32\smartscreen.exe" /reset
+
+NSudo.exe -U:T icacls "%systemroot%\System32\smartscreen.exe" /setowner *S-1-5-80-956008885-3418522649-1831038044-1853292631-2271478464
